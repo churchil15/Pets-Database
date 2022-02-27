b52b2410d0a2e86fc095450db59969bf5e3cb07d
Churchil Yash Rajpal <85837940+churchil15@users.noreply.github.com>
2022-02-27 22:18:23 +0530

    Update PetCursorAdapter.java

--- app/src/main/java/com/example/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/pets/PetCursorAdapter.java
@@ -12,45 +12,32 @@ import android.widget.TextView;
 import data.PetContract;
 
 
-// PetCursorAdapter is an adapter for a list or grid view
- // that uses a Cursor of pet data as its data source. This adapter knows
- // how to create list items for each row of pet data in the Cursor.
 public class PetCursorAdapter extends CursorAdapter {
 
     public PetCursorAdapter(Context context, Cursor c){
         super(context, c, 0);
     }
 
-     // Makes a new blank list item view. No data is set (or bound) to the views yet.
      @Override
      public View newView(Context context, Cursor cursor, ViewGroup parent) {
          return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
      }
 
-     // This method binds the pet data (in the current row pointed to by cursor) to the given
-     // list item layout. For example, the name for the current pet can be set on the name TextView
-     // in the list item layout.
      @Override
      public void bindView(View view, Context context, Cursor cursor) {
-         // Find fields to populate in inflated template (list_item layout)
          TextView nameTextView = (TextView) view.findViewById(R.id.name);
          TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
 
-         // Find the columns of pet attributes that we're interested in
          int nameColumnIndex = cursor.getColumnIndex(PetContract.PetsEntry.COLUMN_PET_NAME);
          int breedColumnIndex = cursor.getColumnIndex(PetContract.PetsEntry.COLUMN_PET_BREED);
 
-         // Read the pet attributes from the Cursor for the current pet
          String petName = cursor.getString(nameColumnIndex);
          String petBreed = cursor.getString(breedColumnIndex);
 
-         // If the pet breed is empty string or null, then use some default text
-         // that says "Unknown breed", so the TextView isn't blank.
          if (TextUtils.isEmpty(petBreed)) {
              petBreed = context.getString(R.string.unknown_breed);
          }
 
-         // Populate fields with extracted properties
          nameTextView.setText(petName);
          summaryTextView.setText(petBreed);
 
